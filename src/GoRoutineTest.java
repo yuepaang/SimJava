65485ce26bf423be60bf03f256cdb192dc0b0f19
YuePeng <py0814@outlook.com>
2019-04-25 20:56:02 +0800

    0425

---
+++ src/GoRoutineTest.java
@@ -0,0 +1,7 @@
+import com.simjava.core.GoRoutines;
+import com.simjava.core.SendOnlyChannel;
+import com.simjava.core.ReceiveOnlyChannel;
+import com.simjava.core.Channel;
+
+public class GoRoutineTest {
+}
