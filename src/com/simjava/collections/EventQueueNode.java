7cd69b219adb995f53e84ca0eea73f17d8d8d20e
Yue Peng <yuepeng@sf-express.com>
2019-04-28 17:17:46 +0800

    revise eventqueue

--- src/com/simjava/collections/EventQueueNode.java
+++ src/com/simjava/collections/EventQueueNode.java
@@ -4,9 +4,9 @@ import com.simjava.core.Event;
 
 public class EventQueueNode {
 
-    private int primaryPriority;
+    int primaryPriority;
 
-    private int secondaryPriority;
+    int secondaryPriority;
 
     public Event event;
 
@@ -22,43 +22,4 @@ public class EventQueueNode {
         this.queueIndex = queueIndex;
     }
 
-    public int getPrimaryPriority() {
-        return primaryPriority;
-    }
-
-    public void setPrimaryPriority(int primaryPriority) {
-        this.primaryPriority = primaryPriority;
-    }
-
-    public int getSecondaryPriority() {
-        return secondaryPriority;
-    }
-
-    public void setSecondaryPriority(int secondaryPriority) {
-        this.secondaryPriority = secondaryPriority;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public void setEvent(Event event) {
-        this.event = event;
-    }
-
-    public long getInsertionIndex() {
-        return insertionIndex;
-    }
-
-    public void setInsertionIndex(long insertionIndex) {
-        this.insertionIndex = insertionIndex;
-    }
-
-    public int getQueueIndex() {
-        return queueIndex;
-    }
-
-    public void setQueueIndex(int queueIndex) {
-        this.queueIndex = queueIndex;
-    }
 }
